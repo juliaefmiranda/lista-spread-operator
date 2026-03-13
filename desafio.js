d6762f1da31aa641921baa852fe27e02a749fddd
Julia Miranda <jerlo180908@gmail.com>
2026-03-13 08:53:40 -0300

    Add files via upload

---
+++ desafio.js
@@ -0,0 +1,6 @@
+//Sistema simples de lista de tarefas
+let tarefas = ["Estudar", "Treinar"];
+let tarefas1 = [...tarefas, "Programar"];
+let tarefas2 = [...tarefas1, "Assistir aula"];
+
+console.log(tarefas2);
